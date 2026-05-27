30e42d8ac333f02161902ffd2f2ff667ed9c0d13
Muhammad Younus Baig <younus@Muhammads-MacBook-Pro.local>
2026-05-27 23:56:53 +0500

    fix tests

--- tests/Lumen.spec.js
+++ tests/Lumen.spec.js
@@ -32,12 +32,11 @@ test('Automate the add to cart order in E-commerce', async({page})=>
     await at.goTo();
     await page.waitForLoadState("load");
     await at.signIn();
-    await Promise.all([
-        page.waitForURL('**/product/**'),
-        at.navigateShop()
-    ]);
+    await at.navigateShop()
+
     const addToCart = page.getByRole('button', { name: 'Add to Cart' });
     await expect(addToCart).toBeVisible();
+    await expect(addToCart).toBeEnabled();
     await addToCart.click();
     const cart = page.locator('a[href="/cart"]');
     await expect(cart.first()).toContainText(/1|2|3/);
